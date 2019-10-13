729be97ea842e44d4e00c2bfd93da1a3e8ee12e3
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2019-10-13 12:54:11 -0300

    Aula 19 e 20

--- Seção3/numbersCuidados.js
+++ Seção3/numbersCuidados.js
@@ -1,6 +1,6 @@
 console.log(77/0)//saida é um infinito
-console.log("55"/2) ///Js permitir fazer operações entre strings e numeros
-console.log('Estudar .....'*12) //
+console.log("55"/2) 
+console.log('Estudar .....'*12)
 console.log(0.7+0.1)
 //console.log(10.toString())
 console.log((425).toString())
