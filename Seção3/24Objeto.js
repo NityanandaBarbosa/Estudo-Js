e446c2389cd8bd6fd978ee5f1b92c7cbbdd8d49f
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2019-10-13 19:45:09 -0300

    Aula 24 e 25

---
+++ Seção3/24Objeto.js
@@ -0,0 +1,14 @@
+const Pessoa = {}
+Pessoa.nome = "Matheus"
+Pessoa.idade = 23
+Pessoa.escolaridade = "Superior em andamento"
+Pessoa['Cidade'] = "Fortaleza" //Evitar o uso
+console.log(Pessoa)
+
+const Pessoa2 = {
+    nome : "Marvinzada",
+    idade : 20,
+    escolaridade : "Superior em andamento",
+    Cidade : "Fortaleza"
+}
+console.log(Pessoa2)
