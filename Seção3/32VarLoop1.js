18a0d79bbb59f6cb053079c6c53750179fb54fa5
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2019-10-14 21:46:43 -0300

    Aula 32-35(Loop Var e Let)

---
+++ Seção3/32VarLoop1.js
@@ -0,0 +1,4 @@
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
+console.log(i) //variavel ainda vista por ser um var
---
+++ Seção3/35LetLoop2.js
@@ -0,0 +1,9 @@
+let vetor = []
+
+for(let i=0; i<10; i++){
+    vetor.push(function(){
+        console.log(i)
+    })
+}
+vetor[2]()
+vetor[8]()
